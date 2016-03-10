3ca2d8f2f3b5ae190e228e146d8b0ce2d51f76bf
Andrew M Olney <aolney@memphis.edu>
2016-03-10 12:43:44 -0600

    initial commit; used ProjectScaffold from fsprojects.github.io

---
+++ src/GuruTutor/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("GuruTutor")>]
+[<assembly: AssemblyProductAttribute("GuruTutor")>]
+[<assembly: AssemblyDescriptionAttribute("This is a redesign and extension of the original Guru project funded by the Institute of Education Sciences from 2008-20edesign incorporates the previous GnuTutor project and represents the next stage of that project. The name change to GuruTutor reflects that the name GnuTutor is no longer appropriate for code that does not use a GPL license.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
